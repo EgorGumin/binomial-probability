a0fc5d4aef1d3c7dc932e45ae104211d2f869e2a
Gumin Egor <gumin.egor@gmail.com>
2019-03-16 01:59:03 +0300

    Fixed floating-point numbers comparison that was done by === operator in tests

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-const binomialProbability = require('./')
-const tap = require('tap')
+const binomialProbability = require('./');
+const tap = require('tap');
 
-tap.equal(binomialProbability(2, 2, 1/2)
-	, 0.25
-  , 'The probability of two coin flips being heads is 0.25')
+const EPSILON = 0.00001;
 
-tap.equal(binomialProbability(5, 2, 1/6)
-  , 0.1607510288065844
-  , 'The probability of getting two sixes when rolling a die twice is roughly 0.161')
+tap.assert(Math.abs(binomialProbability(2, 2, 1/2) - 0.25000) < EPSILON,
+	'The probability of two coin flips being heads is 0.25');
 
-tap.equal(binomialProbability.cumulative(100, 45, 1/2)
-  , 0.184100808663348
-  , 'The probability of getting 45 or fewer heads in 100 tosses of a coin is roughly 0.184')
+tap.assert(Math.abs(binomialProbability(5, 2, 1/6) - 0.16075) < EPSILON,
+	'The probability of getting two sixes when rolling a die twice is roughly 0.161');
+
+tap.assert(Math.abs(binomialProbability.cumulative(100, 45, 1/2)- 0.18410) < EPSILON,
+	'The probability of getting 45 or fewer heads in 100 tosses of a coin is roughly 0.184');
